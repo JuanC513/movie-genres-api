9a07d109e3ce243919c0d3aa819cb15f8b33801a
JuanC513 <juan.esteban.clavijo@correounivalle.edu.co>
2025-06-19 10:02:25 -0500

    Adding json data to work with, and genre model with methods in models

---
+++ models/genre.js
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, '../data/genres.json');
+
+export function getAllGenres() {
+    const data = fs.readFileSync(filePath,'utf8');
+    return JSON.parse(data);
+};
+
+export function getGenreById(id) {
+    const genres = getAllGenres();
+    return genres.find(g => g.id = id);
+};
+
+export function addGenre(genre) {
+    const genres = getAllGenres();
+    genre.id = genres.length ? Math.max(...genres.map(g => g.id)) + 1 : 1;
+    genres.push(genre);
+    fs.writeFileSync(filePath, JSON.stringify(genres, null, 2));
+    return genre;
+};
+
+export function updateGenre(id, updatedGenre) {
+    const genres = getAllGenres();
+    const index = genres.findIndex(g => g.id === id);
+    if (index === -1) return null;
+
+    genres[index].name = updatedGenre.name;
+    fs.writeFileSync(filePath, JSON.stringify(genres, null, 2));
+    return genres[index];
+};
+
+export function deleteGenre(id) {
+    const genres = getAllGenres();
+    const genre = genres.find(g => g.id === id);
+    if (genre === -1) return null;
+
+    const newGenres = genres.filter(g => g.id !== id);
+    fs.writeFileSync(filePath, JSON.stringify(newGenres, null, 2));
+    return genre;
+};
